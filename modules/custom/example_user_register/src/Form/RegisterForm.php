93da41bb5745b0a70dd53fa8054fcc9928ce9db0
ntsanq <nguyenthanhsang2401@gmail.com>
2022-04-20 23:32:43 +0700

    Update: add age validate and fix name validate

--- modules/custom/example_user_register/src/Form/RegisterForm.php
+++ modules/custom/example_user_register/src/Form/RegisterForm.php
@@ -3,18 +3,13 @@ namespace Drupal\register\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Database\Database;
-use \Drupal\Core\Database\Connection;
+
 class RegisterForm extends FormBase {
 	public function getFormId() {
     return 'register_form';
   }
 
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form['description'] = [
-      '#type' => 'item',
-      '#markup' => $this->t('Please enter the title and accept the terms of use of the site.'),
-    ];
 
     $form['name'] = array(
       '#type' => 'textfield',
@@ -23,23 +18,32 @@ class RegisterForm extends FormBase {
     );
     $form['phone'] = array(
       '#type' => 'tel',
-      '#title' => t('Enter Contact Number'),
+      '#title' => t('Enter Contact Number:'),
+      '#required' => TRUE,
     );
     $form['mail'] = array(
       '#type' => 'email',
       '#title' => t('Enter Email:'),
-      '#required' => TRUE,
     );
     $form['age'] = array(
       '#type' => 'select',
       '#title' => t('Select your age:'),
-      '#required' => TRUE,
       '#options' => [
-        '10-20' => $this->t('10-20'),
+        '10-20' => [
+            '10-17' => $this->t('10-17'),
+            '18-20' =>  $this->t('18-20'),
+        ],
         '20-30' => $this->t('20-30'),
         '30-50' => $this->t('30-50'),
       ],
-    ); 
+      '#default' => '10-20',
+    );
+
+    $form['descript'] = array(
+      '#type' => 'textarea',
+      '#title' => $this
+        ->t('Describe yourself:'),
+    );
 
     $form['actions']['#type'] = 'actions';
     $form['actions']['submit'] = array(
@@ -53,16 +57,16 @@ class RegisterForm extends FormBase {
 
   public function validateForm(array &$form, FormStateInterface $form_state) {
 
-    $pattern = '/(^[A-Za-z]{1,16})([ ]{0,1})([A-Za-z]{1,16})?([ ]{0,1})?([A-Za-z]{1,16})?([ ]{0,1})?([A-Za-z]{1,16})/';
+    $pattern = '/^[a-zA-Z]+(?:\s[a-zA-Z]+)+$/';
     $subject = $form_state->getValue('name');
     if(!preg_match($pattern, $subject, $matches)) {
-      $form_state->setErrorByName('name', $this->t('Use your real name, OK?'));
+      $form_state->setErrorByName('name', $this->t('Use your full real name, please'));
     }
 
     $pattern = '/((09|03|07|08|05|01)+([0-9]{8,9})\b)/';
     $subject = $form_state->getValue('phone');
     if(!preg_match($pattern, $subject, $matches)) {
-      $form_state->setErrorByName('phone', $this->t('Please enter a valid vietnamese number'));
+      $form_state->setErrorByName('phone', $this->t('Enter a valid vietnamese number'));
     }
 
     $pattern = '/^\w+\@kyanon.digital$/';
@@ -70,14 +74,12 @@ class RegisterForm extends FormBase {
     if (!preg_match($pattern, $subject, $matches)){
       $form_state->setErrorByName('mail', $this->t('Your mail must in @kyanon.digital'));
     }
-  }
 
+    if ($form_state->getValue('age') == '10-17'){
+      $form_state->setErrorByName('age', $this->t('Your age is under 18, go tell your mom to get registration for you'));
+    }
+  } 
 
-// regex for mail : ^\w+\@kyanon.digital$
-
-
-
-///we can add the data to database from here
   public function submitForm(array &$form, FormStateInterface $form_state) {
 
     $field = $form_state->getValues();
@@ -92,22 +94,19 @@ class RegisterForm extends FormBase {
       'mail'   => $mail,
       'age'   => $age,
     ];
-
-    // $conn = Database::getConnection();
-    // $query = \Drupal::database();
     
-    $query = db_insert('kyanon_register')
-      ->fields($field)
-      ->execute();
+    // $query = db_insert('kyanon_register')
+    //   ->fields($field)
+    //   ->execute();
 
-    drupal_set_message("Succesfully updated");
+    // drupal_set_message("Your infomation are succesfully created");
 
    //  $form_state->setRedirect('register.register_form');
 
-   //  \Drupal::messenger()->addMessage(t("Registration Done!! Registered Values are:"));
-   //  foreach ($form_state->getValues() as $key => $value) {
-   //   \Drupal::messenger()->addMessage($key . ': ' . $value);
-   // }
+    \Drupal::messenger()->addMessage(t("Registration Done!! Registered Values are:"));
+    foreach ($form_state->getValues() as $key => $value) {
+     \Drupal::messenger()->addMessage($key . ': ' . $value);
+    }
  }
 
 }
